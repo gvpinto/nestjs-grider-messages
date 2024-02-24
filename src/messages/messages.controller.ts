a19a25763c28903a6cdd9cb258e98fa03a536556
Piercing Stripes <gvpinto@gmail.com>
2024-02-24 12:42:32 -0500

    Section 4. Adding validation rules. installed class validator and transformer packages

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -6,7 +7,7 @@ export class MessagesController {
   listMessages(): void {}
 
   @Post()
-  createMessage(@Body() body: any): void {
+  createMessage(@Body() body: CreateMessageDto): void {
     console.log(body);
   }
 
