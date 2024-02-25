8c114b58da590e1ce75347f8d7defd1f593aba8a
Piercing Stripes <gvpinto@gmail.com>
2024-02-25 14:43:48 -0500

    Section 5. Reporting exceptions with NestJS exceptions

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,37 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+  constructor() {
+    // Don't do this
+    this.messagesService = new MessagesService();
+  }
   @Get()
-  listMessages(): void {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
-  createMessage(@Body() body: CreateMessageDto): void {
-    console.log(body);
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string): void {
-    console.log(id);
+  async etMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
+    return message;
   }
 }
